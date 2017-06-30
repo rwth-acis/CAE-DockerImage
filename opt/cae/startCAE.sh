613dfb605c4befc24378db6d59e9076ffcc00951
Jonas Könning <jonas.koenning@rwth-aachen.de>
2017-06-30 10:51:22 +0200

    removed bootstrap param

--- opt/cae/startCAE.sh
+++ opt/cae/startCAE.sh
@@ -1,4 +1,4 @@
 echo "Starting CAE Services"
 cd CAE
-screen -S CAE -d -m java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9080 --observer -b las2peer.dbis.rwth-aachen.de:9080  "startService('i5.las2peer.services.modelPersistenceService.ModelPersistenceService@0.1')" "startService('i5.las2peer.services.codeGenerationService.CodeGenerationService@0.1')" startWebConnector interactive
+screen -S CAE -d -m java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9080 "startService('i5.las2peer.services.modelPersistenceService.ModelPersistenceService@0.1')" "startService('i5.las2peer.services.codeGenerationService.CodeGenerationService@0.1')" startWebConnector interactive
 cd ..
