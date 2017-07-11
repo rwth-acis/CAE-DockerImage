8531c775cfaf1fab40c39cf4caad330500d3cf0a
Jonas Könning <jonas.koenning@rwth-aachen.de>
2017-07-11 22:42:58 +0200

    Start supervisor and use configs

--- opt/startup.sh
+++ opt/startup.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+# Make sure service is running
 service supervisor restart
+# Reread configs
+supervisorctl reread
+# Enact changes
+supervisorctl update
 
-http-server -p 80 &
-DEBUG=y*,-y:connector-message y-websockets-server --port 1234 &
+#http-server -p 80 &
+#DEBUG=y*,-y:connector-message y-websockets-server --port 1234 &
 
-cd ROLE/role-m10-sdk
-chmod +x bin/start.sh
+#cd ROLE/role-m10-sdk
+#chmod +x bin/start.sh
 #chmod -R 777 ../role-m10-sdk/
-echo "ROLE"
-sh bin/start.sh &
-echo "Loading spaces"
-echo "CAE"
-chmod +x /startCAE.sh
-cd /
-sh /startCAE.sh
+#echo "ROLE"
+#sh bin/start.sh &
+#echo "Loading spaces"
+#echo "CAE"
+#chmod +x /startCAE.sh
+#cd /
+#sh /startCAE.sh
