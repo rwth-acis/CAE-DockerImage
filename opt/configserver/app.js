bf220c72f13b8a2b43a0c849bcf4e97db2e7ef04
Jonas Könning <jonas.koenning@gmail.com>
2017-07-20 11:10:21 +0200

    Initial configserver

---
+++ opt/configserver/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.post('/upload', function(req, res) {
+    var form = new formidable.IncomingForm();
+    form.multiples = true;
+    form.uploadDir = path.join(__dirname, '/uploads');
+    form.on('file', function(field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+    });
+    form.on('end', function() {
+        res.end('success');
+    });
+
+    form.parse(req);
+
+});
+
+var server = app.listen(3000, function(){
+    console.log('Server listening on port 3000');
+});
