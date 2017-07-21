7b55f7cb3831a75ef1fe605375ad7d0e3feba029
Jonas Könning <jonas.koenning@gmail.com>
2017-07-21 13:52:56 +0200

    live status

--- opt/configserver/public/javascript/status.js
+++ opt/configserver/public/javascript/status.js
@@ -1,15 +1,22 @@
 $( document ).ready(function() {
-  $.ajax({
-    url: '/status',
-    type: 'GET',
-    success: function(data){
-      console.log(data);
-      var text = "";
-      data.content.forEach(function(element) {
-          $("#statusArea").append(`<p>${element}</p>`);
-      }, this);
+
+    function getStatus() {
+        $.ajax({
+            url: '/status',
+            type: 'GET',
+            success: function (data) {
+                console.log(data);
+                var text = "";
+                $("#statusArea").empty();
+                data.content.forEach(function (element) {
+                    $("#statusArea").append(`<p>${element}</p>`);
+                }, this);
+            }
+        });
+        setTimeout(getStatus,5000);
     }
-  });
+
+    getStatus();
 });
 
 $('.upload-btn').on('click', function (){
